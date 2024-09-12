36347b1038cc14a253b1486655c7661cbb428c2f
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-09-12 23:26:19 +0300

    chore: inject `ProductAdapter` into `Entities`

--- Package.swift
+++ Package.swift
@@ -15,11 +15,17 @@ let package = Package(
             targets: ["BrandUseCase"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/obadasemary/BrandRemoteImage.git", .upToNextMajor(from: "1.0.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BrandUseCase"
+            name: "BrandUseCase",
+            dependencies: [
+                "BrandRemoteImage"
+            ]
         ),
         .testTarget(
             name: "BrandUseCaseTests",
